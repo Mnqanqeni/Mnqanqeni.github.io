35f5ea5be5dcfa313a0fb32823f8832c3c63fea6
Ndiyakholwa Mnqanqeni <mnqanqenindiyakholwa206@gmail.com>
2023-12-08 07:24:39 +0200

    add changes

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text-loop", {
+  strings: ["Web Developer", "Software Engineer", "Eagerlearner"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelayed: 1000,
+  loop: true,
+});
