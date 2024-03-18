bb59ef7dc3af14265369b93189f63c08728a994d
Mnqanqeni <ndiyakholwa.mnqanqeni@umuzi.org>
2024-03-19 00:12:24 +0200

    check changes

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 var typed = new Typed(".text-loop", {
-  strings: ["Web Developer", "Software Engineer", "Eagerlearner"],
+  strings: ["Web Developer", "Software Engineer"],
   typeSpeed: 100,
   backSpeed: 50,
   backDelayed: 1000,
   loop: true,
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('.contact-form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    fetch(form.action, {
+      method: form.method,
+      body: new FormData(form)
+    })
+    .then(response => {
+      console.log("Email sent successfully!");
+      form.reset();
+    })
+    .catch(error => {
+      console.error("Error sending email:", error);
+    });
+  });
 });
