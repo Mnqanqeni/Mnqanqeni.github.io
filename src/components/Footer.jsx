500f4ee174e1b8394ef3cadf442ac5133a0adf48
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2025-04-30 22:45:37 +0200

    inital react-tailwind push

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (<div>
+            <div className="container mx-auto flex justiy-between items-center">
+                <h1 className="text-2xl font-bold">footer</h1>
+            </div>
+
+    </div>
+    )
+}
