61471ef98fec2c6b3f34a5f88723702e6d60c3aa
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2025-05-01 05:31:02 +0200

    Addressing hero page and testing deployed version

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import heroImage from '../assets/images/hero.png';
+import heroImage from '../assets/images/hero2.png';
 export default function Hero() {
 
     return (
-        <div className="">
-            <img src={heroImage}/>
+        <div className="py-8 px-7">
+            <div className="">
+                <h1 className="text-secondary">HI I'm Ndiyakholwa Mnqanqeni</h1>
+                <h2>Junior<span id="career"></span></h2>
+            </div>
+            <img src={heroImage} />
         </div>
     );
 }
