d303ab308cdd278b353835ca2454cc2e76976d51
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2025-05-01 06:10:21 +0200

    addressing hero page

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import Typed from "typed.js";
-import heroImage from '../assets/images/hero2.png';
+import heroImage from '../assets/images/hero22.png';
 
 export default function Hero() {
     const el = useRef(null); // Create a ref for the span element
@@ -25,10 +25,13 @@ export default function Hero() {
     }, []);
 
     return (
-        <div className="py-8 px-7">
+        <div className="flex py-8 px-7">
             <div>
                 <h1 className="text-secondary">Hi, I'm Ndiyakholwa Mnqanqeni</h1>
                 <h2>Junior <span ref={el}></span></h2>
+                <button className="bg-secondary text-fourth px-4 py-2 rounded-md mt-4">
+                    <a href="#contact">Contact Me</a>
+                </button>
             </div>
             <img src={heroImage} alt="Hero" />
         </div>
