ad6df61ce0ebe03cfb70ce4c4415e59bde0e9f00
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2025-05-01 05:53:07 +0200

    Addressing hero page and testing deployed version

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
 import heroImage from '../assets/images/hero2.png';
+
 export default function Hero() {
+    const el = useRef(null); // Create a ref for the span element
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: [
+                "Fullstack Developer",
+                "Software Engineer",
+                "Cyber Security Enthusiast",
+            ],
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 3000,
+            loop: true,
+        });
+
+        // Destroy Typed instance during cleanup to prevent memory leaks
+        return () => {
+            typed.destroy();
+        };
+    }, []);
 
     return (
         <div className="py-8 px-7">
-            <div className="">
-                <h1 className="text-secondary">HI I'm Ndiyakholwa Mnqanqeni</h1>
-                <h2>Junior<span id="career"></span></h2>
+            <div>
+                <h1 className="text-secondary">Hi, I'm Ndiyakholwa Mnqanqeni</h1>
+                <h2>Junior <span ref={el}></span></h2>
             </div>
-            <img src={heroImage} />
+            <img src={heroImage} alt="Hero" />
         </div>
     );
 }
