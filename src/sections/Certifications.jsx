9c08180c938a944a23283c9df23c0d2c9bfc38a3
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2025-05-02 07:01:07 +0200

    making dummy pages

--- src/sections/Certifications.jsx
+++ src/sections/Certifications.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Certification() {
     return (
-        <section className="flex flex-col items-center justify-between gap-4 md:px-12 pt-14 md:pt-60 animate-fade-in-up">
-            <div className="text-3xl font-bold">
-                <h1>My <span className="text-secondary">Certifications</span></h1>
+        <section className="flex flex-col items-center justify-between">
+            <div className="text-3xl font-bold py-16">
+                <h1> <span className="text-secondary">My </span>Certifications</h1>
             </div>
             <div className="w-full h-1-6 bg-gradient-frame-line" />
         </section>
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -2,10 +2,35 @@ import React from "react";
 
 export default function Contact() {
     return (
-        <section className="flex flex-col items-center justify-between gap-4 md:px-12 pt-14 md:pt-60 animate-fade-in-up">
-            <div className="text-3xl font-bold">
-                <h1>Contact <span className="text-secondary">Me</span></h1>
+        <section className="flex flex-col items-center justify-between relative">
+            <div className="py-16 ">
+                <div className="flex flex-col items-center text-3xl font-bold ">
+                    <h1>Contact <span className="text-secondary">Me</span></h1>
+                    <p className="text-[#ffffffb2] text-xs mt-2">
+                    I’m open to collaborations — reach out below!
+                    </p>
+                </div>
+                <form className="flex flex-col items-center mt-8">
+                    <input
+                        type="text"
+                        placeholder="Your Name"
+                        className="border-2 border-gray-300 rounded-lg p-2 mb-4 w-64"
+                    />
+                    <input
+                        type="email"
+                        placeholder="Your Email"
+                        className="border-2 border-gray-300 rounded-lg p-2 mb-4 w-64"
+                    />
+                    <textarea
+                        placeholder="Your Message"
+                        className="border-2 border-gray-300 rounded-lg p-2 mb-4 w-64 h-32"
+                    />
+                    <button className="bg-secondary text-white py-2 px-4 rounded-lg">
+                        Send Message
+                    </button>
+                </form>
             </div>
+
             <div className="w-full h-1-6 bg-gradient-frame-line" />
         </section>
     );
