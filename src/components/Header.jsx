3fb41e611ba943f50617f45b9e51d2862f642fc7
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2025-05-03 19:22:27 +0200

    dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,9 @@ import logo from '../assets/logos/logo.png';
 import navbar from "../assets/logos/bars-solid.svg";
 
 export default function Header() {
+    const [dropdownOpen, setDropdownOpen] = React.useState(false);
+    const toggleDropdown = () => setDropdownOpen(prev => !prev);
+  
     return (
         <header>
             <nav className="flex fixed justify-between items-center p-2 shadow-navShadow z-50 bg-primary w-full">
@@ -14,7 +17,15 @@ export default function Header() {
                     </div>
                 </div>
                 <div>
-                    <img src={navbar} alt="Menu" className="md:hidden h-9 cursor-pointer" />
+                    { !dropdownOpen && (
+                        <img
+                            src={navbar}
+                            alt="Navbar Icon"
+                            className="w-8 h-8 md:hidden cursor-pointer"
+                            onClick={toggleDropdown}
+                        />
+                    )}
+                    
                     <div>
                         <ul className="hidden md:flex gap-5 text-fourth font-playfair">
                             <li className="hover:text-secondary cursor-pointer">Home</li>
@@ -29,6 +40,20 @@ export default function Header() {
                     </div>
                 </div>
             </nav>
+            {dropdownOpen && (
+                        <div className="w-full h-full absolute left-0 top-16 bg-red-900 rounded-md p-4">
+                            <ul className="flex flex-col items-center gap-5 text-fourth font-playfair">
+                                <li className="hover:text-secondary cursor-pointer" onClick={toggleDropdown}>X</li>
+                                <li className="hover:text-secondary cursor-pointer">Home</li>
+                                <li className="hover:text-secondary cursor-pointer">About</li>
+                                <li className="hover:text-secondary cursor-pointer">Projects</li>
+                                <li className="hover:text-secondary cursor-pointer">Skills</li>
+                                <li className="hover:text-secondary cursor-pointer">Certifications</li>
+                                <li className="hover:text-secondary cursor-pointer">Experience</li>
+                                <li className="hover:text-secondary cursor-pointer">Contact</li>
+                            </ul>
+                        </div>
+                    )}
         </header>
     );
 }
