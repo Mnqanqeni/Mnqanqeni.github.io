6189b2e4804871f41960387efa88fded9c2818ff
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2025-05-03 19:50:08 +0200

    dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import logo from '../assets/logos/logo.png';
-import navbar from "../assets/logos/bars-solid.svg";
+import navbar from "../assets/icons/bars-solid.svg";
+import xIcon from "../assets/icons/xmark-solid.svg";
 
 export default function Header() {
     const [dropdownOpen, setDropdownOpen] = React.useState(false);
     const toggleDropdown = () => setDropdownOpen(prev => !prev);
   
     return (
-        <header>
+        <header className="w-full flex flex-col items-center">
             <nav className="flex fixed justify-between items-center p-2 shadow-navShadow z-50 bg-primary w-full">
                 <div className="flex items-center gap-3 cursor-pointer">
                     <img src={logo} alt="Logo" className="h-12" />
@@ -24,8 +25,17 @@ export default function Header() {
                             className="w-8 h-8 md:hidden cursor-pointer"
                             onClick={toggleDropdown}
                         />
+
                     )}
-                    
+                    { dropdownOpen && (
+                        <img
+                            src={xIcon}
+                            alt="Navbar Icon"
+                            className="w-8 h-13 md:hidden cursor-pointer"
+                            onClick={toggleDropdown}
+                        />
+                    )}
+
                     <div>
                         <ul className="hidden md:flex gap-5 text-fourth font-playfair">
                             <li className="hover:text-secondary cursor-pointer">Home</li>
@@ -41,9 +51,8 @@ export default function Header() {
                 </div>
             </nav>
             {dropdownOpen && (
-                        <div className="w-full h-full absolute left-0 top-16 bg-red-900 rounded-md p-4">
+                        <div className="w-1/2 md:hidden h-auto absolute right-0 top-16 bg-dark-gradient rounded-md p-4">
                             <ul className="flex flex-col items-center gap-5 text-fourth font-playfair">
-                                <li className="hover:text-secondary cursor-pointer" onClick={toggleDropdown}>X</li>
                                 <li className="hover:text-secondary cursor-pointer">Home</li>
                                 <li className="hover:text-secondary cursor-pointer">About</li>
                                 <li className="hover:text-secondary cursor-pointer">Projects</li>
