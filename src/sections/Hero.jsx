83b81cbb12eaaca74d57b696c518f1dde3817296
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2025-05-04 17:43:24 +0200

    addressing hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,7 +28,7 @@ export default function Hero() {
 
     return (
         <section className="flex flex-col items-center justify-between gap-4 pt-28  md:pt-44  px-4">
-            <div flxex className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center">
                 <div>
                     <h1 className="text-fourth text-2xl md:text-4xl font-bold">
                         Hi, I'm Ndiyakholwa Mnqanqeni
@@ -39,8 +39,8 @@ export default function Hero() {
                 </div>
                 <div>
                     <div className="flex flex-row  w-full justify-center items-center gap-4 mt-6">
-                        <p className="flex-1 text-fourth text-sm">
-                            I am a passionate full-stack developer with a keen interest in software engineering and cyber security. I specialize in creating dynamic and responsive web applications, and I am always eager.
+                        <p className="flex-1 text-fourth text-base">
+                        I'm a passionate full-stack developer with a strong interest in software engineering and cybersecurity. I specialize in building dynamic, responsive web applications and am always eager to learn and grow in the tech industry.
                         </p>
                         <div className="flex-1 flex justify-center items-center relative">
                             <div className="h-full w-full bg-gradient-to-tr from-transparent via-black/30 shadow-customBlue to-cyan-500/80 flex justify-center items-center z-10">
@@ -52,29 +52,29 @@ export default function Hero() {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col gap-4 mt-14 w-full">
-                    <div className="flex flex-row items-center justify-between gap-2">
+                <div className="flex flex-col items-center justify-between gap-4 mt-14 w-full">
+                    <div className="flex flex-row items-center justify-between gap-2 w-full">
                         <button className="bg-accent text-white px-4 bg-secondary py-2 rounded-md hover:bg-accent-dark transition duration-300">
-                            <a href="#projects">Contact Me</a>
+                        Get in Touch
                         </button>
                         <div className="flex gap-4">
                             <a href="https://github.com/Mnqanqeni/Mnqanqeni" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
-                                <img src={githubIcon} alt="GitHub Profile" className="w-6 h-6 hover:scale-110 transition-transform" />
+                                <img src={githubIcon} alt="GitHub Profile" className="w-10 h-10 hover:scale-110 transition-transform" />
                             </a>
                             <a href="https://www.linkedin.com/in/ndiyakholwamnqanqeni" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-                                <img src={linkedinIcon} alt="LinkedIn Profile" className="w-6 h-6 hover:scale-110 transition-transform" />
+                                <img src={linkedinIcon} alt="LinkedIn Profile" className="w-10 h-10 hover:scale-110 transition-transform" />
                             </a>
                             <a href="https://twitter.com/Mnqanqeni" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-                                <img src={twitterIcon} alt="Twitter Profile" className="w-6 h-6 hover:scale-110 transition-transform" />
+                                <img src={twitterIcon} alt="Twitter Profile" className="w-10 h-10 hover:scale-110 transition-transform" />
                             </a>
                         </div>
 
                     </div>
 
                     <div>
-                        <a href={cvFile} download className="flex items-center gap-2 mt-4 text-fourth">
-                            <img src={downloadIcon} alt="Download CV Icon" className="w-5 h-5" />
-                            <p className="text-sm">Download CV</p>
+                        <a href={cvFile} download className="flex justify-center items-end gap-2 mt-4 text-fourth">
+                            <img src={downloadIcon} alt="Download CV Icon" className="w-8 h-8" />
+                            <span className="text-xl h-6">Download My CV</span>
                         </a>
 
                     </div>
