88f3a97f2aa7ecc03aeeae8b3f8f58d7446c7e8b
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2025-05-04 19:41:03 +0200

    addressing contact me

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -10,18 +10,36 @@ export default function About() {
         </h1>
       </div>
 
-      <div className="flex flex-col items-center gap-10 max-w-5xl">
-        <img
-          src={aboutMeImage}
-          alt="About me"
-          className=" h-64 border-2 border-secondary"
-        />
+      <div className="flex flex-col md:flex-row items-center relative justify-center">
+        <div className="flex flex-col relative">
+          <div className="w-[416.55px] h-[443.92px] relative flex left-[0px]">
+            {/* Stackview One */}
+            <div className="h-[326px] w-[326px] rotate-[-48deg] bg-gradient-to-br from-[#00a8cda5] to-transparent absolute left-[15%] rounded-full" />
+
+            {/* Stackview Two */}
+            <div className="h-[162px] w-[162px] bg-gradient-to-br from-[#00a8cda5] to-transparent absolute bottom-[0.39px] rounded-[80px]" />
+
+            {/* Stackview Three */}
+            <div className="absolute inset-0">
+              {/* Inner Stack */}
+              <div className="h-[266.89px] w-[266.89px] bg-gradient-to-b from-[#00a8cd] to-transparent rotate-[5.11deg] flex flex-col absolute bottom-[70px] left-[40px] shadow-[4px_4px_0px_0px_#00a8cd]" />
+
+              {/* Image Container */}
+              <div className="h-[266.89px] w-[266.89px] bg-gradient-to-br from-black to-transparent flex flex-col absolute bottom-[15%] left-[8%]">
+                <img
+                  src={aboutMeImage}
+                  alt="About me"
+                  className="h-[266px] absolute ml-[40px]"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="text-fourth text-lg leading-relaxed max-w-xl">
           I am someone fascinated by complexity and system interactions.
           I enjoy solving challenging problems, building scalable software, and continuously learning how technology shapes the world.
         </div>
       </div>
-
       <div className="w-full h-1-6 bg-gradient-frame-line" />
     </section>
   );
