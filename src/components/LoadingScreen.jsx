5c076fdbdee7bfea4c6351ea16d6aacccc219a16
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2025-05-06 13:18:44 +0200

    fix load screan

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -7,21 +7,20 @@ export default function LoadingScreen(onComplete) {
     
         useEffect(() => {
             const typed = new Typed(el.current, {
-                strings: [
-                    "hello world",
-                ],
-                typeSpeed: 100,
-                backSpeed: 50,
-                backDelay: 3000,
+                strings: ["hello world"],
+                typeSpeed: 80,
+                backSpeed: 40,
+                backDelay: 2000,
                 loop: true,
+                smartBackspace: true,
             });
     
             return () => typed.destroy();
         }, []);
     return (
         <div className="fixed inset-0 flex items-center justify-center flex-col bg-primary text-gray-100  h-screen bg-gradient-to-br from-cyan-500/60 via-black/30 to-transparent">
-        <div className=" text-4xl font-mono font-bold"><span ref={el}> | </span></div>
-        <div className="w-[250px] h-[2px] bg-fourth rounded relative overflow-hidden">
+        <h1 className=" text-4xl font-mono font-bold min-w-[12ch] min-h-[1em]"><span ref={el}> | </span></h1>
+        <div className="w-[255px] h-[3px] bg-fourth rounded relative overflow-hidden">
             <div className="w-full h-full bg-secondary shadow-customBlue animate-loading-bar">
                 {" "}
             </div>
