c403caa611f9953a93c27585d316651721994ae2
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2025-05-06 14:05:25 +0200

    loading

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,48 +1,30 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
+import { useEffect, useRef } from "react";
 import Typed from "typed.js";
 
-export default function LoadingScreen({ onComplete }) {
-  const el = useRef(null);
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    const typed = new Typed(el.current, {
-      strings: ["Welcome!", "Loading..."],
-      typeSpeed: 80,
-      backSpeed: 40,
-      backDelay: 1500,
-      loop: true,
-    });
-
-    return () => typed.destroy();
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setProgress((prev) => {
-        if (prev >= 100) {
-          clearInterval(interval);
-          if (onComplete) onComplete(); // optional callback
-          return 100;
-        }
-        return prev + 1;
-      });
-    }, 50); // progress increases every 50ms
-
-    return () => clearInterval(interval);
-  }, [onComplete]);
-
-  return (
-    <div className="fixed inset-0 flex flex-col items-center justify-center h-screen px-4 bg-gradient-to-br from-cyan-500/60 via-primary to-transparent text-fourth">
-      <h1 className="text-5xl font-mono font-bold mb-8 text-center min-w-[12ch]">
-        <span ref={el}>|</span>
-      </h1>
-      <div className="w-64 h-4 bg-gray-800 rounded-full overflow-hidden shadow-md">
-        <div
-          className="h-full bg-blue-500 transition-all duration-200 ease-linear"
-          style={{ width: `${progress}%` }}
-        />
-      </div>
-    </div>
-  );
-}
+export default function LoadingScreen(onComplete) {
+     const el = useRef(null);
+    
+        useEffect(() => {
+            const typed = new Typed(el.current, {
+                strings: ["Welcome!", "Loading..."],
+                typeSpeed: 80,
+                backSpeed: 40,
+                backDelay: 2000,
+                loop: true,
+                smartBackspace: true,
+            });
+    
+            return () => typed.destroy();
+        }, []);
+    return (
+        <div className="fixed inset-0 flex items-center justify-center flex-col bg-primary text-gray-100  h-screen bg-gradient-to-br from-cyan-500/60 via-black/30 to-transparent">
+        <h1 className="pl-4 text-4xl font-mono font-bold min-w-[12ch] min-h-[1em]"><span ref={el}> | </span></h1>
+        <div className="w-[240px] h-[3px] bg-fourth rounded relative overflow-hidden">
+            <div className="w-full h-full bg-secondary shadow-customBlue animate-loading-bar">
+                {" "}
+            </div>
+        </div>
+        </div>
+    );
+    }
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -93,7 +93,55 @@ const projects = [
     }
   ];
 
-  
+// import React, { useEffect, useRef, useState } from "react";
+// import Typed from "typed.js";
+
+// export default function LoadingScreen({ onComplete }) {
+//   const el = useRef(null);
+//   const [progress, setProgress] = useState(0);
+
+//   useEffect(() => {
+//     const typed = new Typed(el.current, {
+//       strings: ["Welcome!", "Loading..."],
+//       typeSpeed: 80,
+//       backSpeed: 40,
+//       backDelay: 1500,
+//       loop: true,
+//     });
+
+//     return () => typed.destroy();
+//   }, []);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setProgress((prev) => {
+//         if (prev >= 100) {
+//           clearInterval(interval);
+//           if (onComplete) onComplete(); // optional callback
+//           return 100;
+//         }
+//         return prev + 1;
+//       });
+//     }, 50); 
+
+//     return () => clearInterval(interval);
+//   }, [onComplete]);
+
+//   return (
+//     <div className="fixed inset-0 flex flex-col items-center justify-center h-full px-4 bg-gradient-to-br from-cyan-500/60 via-primary to-transparent text-fourth">
+//       <h1 className="text-5xl font-mono font-bold mb-8 text-center min-w-[12ch]">
+//         <span ref={el}>|</span>
+//       </h1>
+//       <div className="w-64 h-4 bg-gray-800 rounded-full overflow-hidden shadow-md">
+//         <div
+//           className="h-full bg-blue-500 transition-all duration-200 ease-linear"
+//           style={{ width: `${progress}%` }}
+//         />
+//       </div>
+//     </div>
+//   );
+// }
+
 
 export default function Skills() {
     return (
