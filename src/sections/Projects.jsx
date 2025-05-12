a3c6a2b52c1787ad3ebe38277f38af8e949ff2a7
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2025-05-12 02:25:46 +0200

    adding UI filters

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -11,45 +11,35 @@ export default function Projects() {
             <div className="text-3xl font-bold xl:text-5xl py-16">
                 <h1> <span className="text-secondary">My </span>Projects</h1>
             </div>
-            <div className="flex flex-col gap-4">
-                <label className="block text-lg text-white">Level:</label>
-                <select id="filter-level" className="p-2 rounded bg-secondary w-full">
-                    <option value="">All Levels</option>
-                    {Object.entries(levels).map((skill, index) => (
-                        <option key={index} value={skill[0].toLowerCase()}>{skill[1]}</option>
-                    ))}
-                </select>
-
-                <div className="flex flex-row gap-4">
-                    <div>
-                        <label className="block text-sm text-white mb-1">Category:</label>
-                        <select id="filter-category" className="p-2 rounded bg-secondary">
+            <div className="flex flex-col gap-4 w-full">
+                <div className="flex flex-row gap-4 max-w-screen-md md:min-w-[700px] self-center">
+                    <div className="flex-1 min-w-0">
+                        <label className="block text-lg text-white">Level:</label>
+                        <select id="filter-level" className="w-full p-2 rounded bg-secondary">
+                            <option value="">All Levels</option>
+                            {Object.entries(levels).map((skill, index) => (
+                                <option key={index} value={skill[0].toLowerCase()}>{skill[1]}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="flex-1 min-w-0">
+                        <label className="block text-lg text-white">Category:</label>
+                        <select id="filter-category" className="w-full p-2 rounded bg-secondary">
                             <option value="">All Categories</option>
                             {Object.entries(categories).map((category, index) => (
                                 <option key={index} value={category[0].toLowerCase()}>{category[1]}</option>
                             ))}
                         </select>
                     </div>
-                    <div>
-                        <label className="block text-sm text-white mb-1">Skills:</label>
-                        <select id="filter-skills" className="p-2 rounded bg-secondary">
-                            <option value="">All Skills</option>
-                            {Object.entries(skillsList).map((skill, index) => (
-                                <option key={index} value={skill[0].toLowerCase()}>{skill[1]}</option>
-                            ))}
-                        </select>
-                    </div>
-                    
-
                 </div>
-                <button
-                        
-                        className="w-full mt-4 py-2 px-4 rounded-md bg-cyan-600 hover:bg-cyan-700 text-white font-medium transition"
-                    >
-                        Clear Filters
-                    </button>
 
+                <button
+                    className="text-fourth px-6 bg-secondary min-w-[10rem] py-2 rounded-md hover:bg-accent-dark transition duration-300 self-center"
+                >
+                    Clear Filters
+                </button>
             </div>
+
             <div className="flex flex-wrap justify-center gap-8">
                 {projects.map((project, index) => (
                     <ProjectCard key={index} project={project} />
